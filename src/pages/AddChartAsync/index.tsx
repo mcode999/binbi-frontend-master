cbbc96b2034d19d19836a82194572296d10856db
ZgBinRs <13093982+zgbinrs@user.noreply.gitee.com>
2023-10-07 15:48:46 +0800

    同步gitee仓库

--- src/pages/AddChartAsync/index.tsx
+++ src/pages/AddChartAsync/index.tsx
@@ -1,4 +1,8 @@
-import {genChartByAiAsyncMqUsingPOST, genChartByAiAsyncUsingPOST} from '@/services/yubi/chartController';
+import {
+  genChartByAiAsyncMqUsingPOST,
+  genChartByAiAsyncUsingPOST,
+  genChartByAiUsingPOST
+} from '@/services/yubi/chartController';
 import {UploadOutlined} from '@ant-design/icons';
 import {Button, Card, Form, Input, message, Select, Space, Upload} from 'antd';
 import {useForm} from 'antd/es/form/Form';
@@ -29,7 +33,8 @@ const AddChartAsync: React.FC = () => {
       file: undefined,
     };
     try {
-      const res = await genChartByAiAsyncUsingPOST(params, {}, values.file.file.originFileObj);
+      const res = await genChartByAiUsingPOST(params, {}, values.file.file.originFileObj);
+      // const res = await genChartByAiAsyncUsingPOST(params, {}, values.file.file.originFileObj);
       // const res = await genChartByAiAsyncMqUsingPOST(params, {}, values.file.file.originFileObj);
       if (!res?.data) {
         message.error('分析失败');
